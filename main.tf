b7a098073f41174faf5c7ee5aca9ae73ead63200
1998arunrekha <arunar16061998@gmail.com>
2022-10-04 13:20:29 +0530

    hhh

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ resource "azurerm_storage_account" "storage_account" {
     resource_group_name = azurerm_resource_group.resource_group.name
     location = azurerm_resource_group.resource_group.location
 
-    access_tier             = "standard"
-    account_replication_type = "LRS"
+    access_tier               = "Standard"
+    account_replication_type  = "LRS"
     account_kind              = "StorageV2"
     enable_https_traffic_only = true
-    allow_blob_public_access = true 
+    allow_blob_public_access  = true
 
     static_website {
       index_document = "index.html"
@@ -21,11 +21,11 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_blob" "example" {
-    name = "index.html"
-    storage_account_name = azurerm_storage_account.storage_account.name
-    storage_container_name = "$web"
-    type = "Block"
-    content_type = "text/html"
-    source_content = "<h1>hey friends, this website was deployed with terraform on azure storage"
+    name                     = "index.html"
+    storage_account_name     = azurerm_storage_account.storage_account.name
+    storage_container_name   = "$web"
+    type                     = "Block"
+    content_type             = "text/html"
+    source_content           = "<h1>hey friends, this website was deployed with terraform on azure storage"
   
 }
